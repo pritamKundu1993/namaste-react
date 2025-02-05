9617577f003239e456f43fa30ef17fac30e4b1dc
pritam kundu <riku.kundu.1993@gmail.com>
2025-02-05 23:44:52 +0530

    add parcel builder to the projects

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
 const headings = React.createElement(
     'h1',
     { id: 'headings', xyz: 'abc' },
-    'React from hello world'
+    'React from hello world🍷⛹️‍♂️'
 );
 console.log(headings);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(headings);
